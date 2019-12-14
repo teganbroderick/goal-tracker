d9cf23162643382e766213dd282034c06160c035
Tegan Broderick <teganbroderick@gmail.com>
2019-12-14 01:56:40 +0000

    Added from model import...

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from jinja2 import StrictUndefined
 from flask import Flask, render_template, redirect, request, flash, session, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
+from model import User, Task, connect_to_db, db
 
 app = Flask(__name__)
 
