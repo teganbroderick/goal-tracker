06e751fec57af8fc5283a83dbfd5724487bdf159
Tegan Broderick <teganbroderick@gmail.com>
2019-12-14 00:26:47 +0000

    Added basic framework

---
+++ model.py
@@ -0,0 +1,20 @@
+from flask_sqlalchemy import SQLAlchemy
+
+def connect_to_db(app, db_uri="postgresql:///todo"):
+    """Connect the database to the Flask app"""
+
+    # Configure to use the database.
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
+    app.config["SQLALCHEMY_ECHO"] = False
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+
+
+if __name__ == "__main__":
+    # As a convenience, if we run this module interactively, it will leave
+    # you in a state of being able to work with the database directly.
+
+    from server import app
+    connect_to_db(app)
+    print("Connected to DB.")
